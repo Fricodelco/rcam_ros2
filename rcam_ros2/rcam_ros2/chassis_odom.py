7d5a1c3d24198d4e03c499ad963136db736e2c01
Eugene Zemskov <ezemskov@yandex.ru>
2021-07-18 21:33:08 +1000

    chassis_odom.py : handling of cmd_vel

--- rcam_ros2/rcam_ros2/chassis_odom.py
+++ rcam_ros2/rcam_ros2/chassis_odom.py
@@ -6,17 +6,21 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 from tf2_ros import TransformBroadcaster
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Vector3
 from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Vector3
 from ChassisInterface import ChassisInterface
 
-class ChassisOdom(Node):
+class ChassisNode(Node):
     WheelBaselineMeters = 0.430
     WheelRadiusMeters = 0.115
     ReportIntervalSec = 0.02
     SecInNsec = 10**-9
-    RadPerSecInWheelFeedback = 1.    
+    RadPerSecInWheelFeedback = 1.
+
+    MaxSpeedMetersSec = 1.0 # linear velocity from vel_cmd is scaled and bounded to this
+    MaxYawRateRadSec = 1.0  # yaw rate from vel_cmd is scaled and bounded to this. todo : set to actual max yaw rate of chassis
 
     def __init__(self):
         super().__init__('chassis_odom')
@@ -26,14 +30,20 @@ class ChassisOdom(Node):
         self.x = 0
         self.y = 0
         self.theta = 0
-        self.timeLast = self.get_clock().now().nanoseconds
+
+        self.cmdSpeed = 0
+        self.cmdSteering = 0
+
+        self.tsFeedback = self.get_clock().now().nanoseconds
         self.chassis = ChassisInterface()
         self.chassis.setWheelCallback(self.processWheelFeedback)
+
+        self.sub = self.create_subscription(Twist, 'cmd_vel', self.processCmd, 10)
         logging.info('chassis_odom initialized')
 
     def processWheelFeedback(self, deviceId, respTuple):
         now = self.get_clock().now()
-        deltaTimeSec = (now.nanoseconds - self.timeLast) * ChassisOdom.SecInNsec
+        deltaTimeSec = (now.nanoseconds - self.tsFeedback) * ChassisNode.SecInNsec
 
         zeroRotation = Quaternion()
         zeroTranslation = Vector3()
@@ -41,14 +51,14 @@ class ChassisOdom(Node):
         zeroTranslation.y = 0.0
         zeroTranslation.z = 0.0
 
-        if deltaTimeSec >= ChassisOdom.ReportIntervalSec:
-            self.timeLast = now.nanoseconds
-            wheelLRadSec = respTuple[0] * ChassisOdom.RadPerSecInWheelFeedback;
-            wheelRRadSec = respTuple[1] * ChassisOdom.RadPerSecInWheelFeedback;
+        if deltaTimeSec >= ChassisNode.ReportIntervalSec:
+            self.tsFeedback = now.nanoseconds
+            wheelLRadSec = respTuple[0] * ChassisNode.RadPerSecInWheelFeedback
+            wheelRRadSec = respTuple[1] * ChassisNode.RadPerSecInWheelFeedback
             Vx, Vy, Vtheta = self.calculate_odom(deltaTimeSec, wheelRRadSec, wheelLRadSec)
 
             logging.debug('chassis_odom wheel feedback : {0} {1} speed {2} yawRate {3} pos [{4} {5}] yaw {6}'.format(
-                wheelLRadSec, wheelRRadSec, Vx, Vtheta, self.x, self.y, self.theta));
+                wheelLRadSec, wheelRRadSec, Vx, Vtheta, self.x, self.y, self.theta))
 
             quaternion = Quaternion()
             quaternion.x = 0.0
@@ -101,14 +111,25 @@ class ChassisOdom(Node):
             transform_stamped_msg.transform.translation.z = 0.4
             self.odom_broadcaster.sendTransform(transform_stamped_msg)
 
+    def processCmd(self, data):
+        #todo : add deadzone for Vx and yaw rate
+        #todo : reset chassis control to zeroes if cmd_vel has expired
+        cmdSpeedBounded = max(min(data.linear.x, MaxSpeedMetersSec), -MaxSpeedMetersSec)  
+        self.cmdSpeed = cmdSpeedBounded / MaxSpeedMetersSec
+        self.chassis.setSpeed(self.cmdSpeed)
+
+        cmdYawRateBounded = max(min(data.angular.z, MaxYawRateRadSec), -MaxYawRateRadSec)
+        self.cmdSteering = cmdYawRateBounded / MaxYawRateRadSec
+        self.chassis.setSteering(self.cmdSteering)
+        logging.debug('chassis_odom cmd speed {0}/{1} steering {2}/{3}'.format(
+            data.linear.x, self.cmdSpeed, data.angular.z, cmdSteering))
+
     def calculate_odom(self, delta, Lvel, Rvel):
         # Lvel= -Lvel
-        vel = (ChassisOdom.WheelRadiusMeters)*(Rvel+Lvel)/2
-        Vx = vel
+        Vx = (ChassisNode.WheelRadiusMeters)*(Rvel+Lvel)/2
         Vy = 0
 
-        # New theta for caclulationg rotation matrix:
-        Vtheta = -1.6*(ChassisOdom.WheelRadiusMeters)*(Rvel-Lvel)/ChassisOdom.WheelBaselineMeters
+        Vtheta = -1.6*(ChassisNode.WheelRadiusMeters)*(Rvel-Lvel)/ChassisNode.WheelBaselineMeters
         # Rotation matrix
         self.theta += delta * Vtheta
         self.y += delta * sin(self.theta)*Vx
@@ -118,7 +139,7 @@ class ChassisOdom(Node):
 
 def main():
     rclpy.init()
-    odom = ChassisOdom()
+    odom = ChassisNode()
     rclpy.spin(odom)
     rclpy.shutdown()
 
